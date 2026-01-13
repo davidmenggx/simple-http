81e3b0ef4a50db2970391bc75ab2c6d25ef74a3f
David Meng <davidmenggx@gmail.com>
2026-01-13 10:00:02 -0800

    Changed to format error response

--- responses.py
+++ responses.py
@@ -1,17 +1,17 @@
 from datetime import datetime, timezone
 
-def _format_response(status_line: str) -> bytes:
+def _format_error_response(status_line: str) -> bytes:
     now = datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
     return (f"{status_line}\r\nServer: David's server\r\nDate: {now}\r\nContent-Length: 0\r\n\r\n").encode('utf-8')
 
-def bad_request(): return _format_response('HTTP/1.1 400 Bad Request')
+def bad_request(): return _format_error_response('HTTP/1.1 400 Bad Request')
 
-def forbidden(): return _format_response('HTTP/1.1 403 Forbidden')
+def forbidden(): return _format_error_response('HTTP/1.1 403 Forbidden')
 
-def not_found(): return _format_response('HTTP/1.1 404 Not Found')
+def not_found(): return _format_error_response('HTTP/1.1 404 Not Found')
 
-def internal_server_error(): return _format_response('HTTP/1.1 500 Internal Server Error')
+def internal_server_error(): return _format_error_response('HTTP/1.1 500 Internal Server Error')
 
-def not_implemented(): return _format_response('HTTP/1.1 501 Not Implemented')
+def not_implemented(): return _format_error_response('HTTP/1.1 501 Not Implemented')
 
-def http_version_not_supported(): return _format_response('HTTP/1.1 505 HTTP Version Not Supported')
+def http_version_not_supported(): return _format_error_response('HTTP/1.1 505 HTTP Version Not Supported')
