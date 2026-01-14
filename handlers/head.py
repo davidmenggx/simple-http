b40a24753a7548469d748a5563de3fb1033340a0
David Meng <davidmenggx@gmail.com>
2026-01-14 09:38:59 -0800

    Refactored path check logic

--- handlers/head.py
+++ handlers/head.py
@@ -13,47 +13,46 @@ def head(path: str, headers: dict[str,str], body: bytes = b'') -> bytes:
 
     requested_path = (BASE_DIR / path).resolve()
 
-    if BASE_DIR in requested_path.parents or BASE_DIR == requested_path:
-        if requested_path.exists() and requested_path.is_file():
-            try:
-                with open(requested_path, 'rb') as f:
-                    try:
-                        content = f.read()
-                        
-                        now = datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
-
-                        current_etag = get_etag(str(requested_path))
-                        user_etag = headers.get('if-none-match')
-                        if user_etag and user_etag[1:-1] == current_etag:
-                            return (f"HTTP/1.1 304 Not Modified\r\nDate: {now}\r\nServer: David's server\r\n\r\n").encode('utf-8')
-
-                        response = (f"HTTP/1.1 200 OK\r\nDate: {now}\r\nDavid's server\r\n")
-                        
-                        if headers.get('connection', '') == 'close':
-                            response += (f'Connection: Close\r\n')
-                        else:
-                            response += (f'Connection: Keep-Alive\r\n')
-                        
-                        content_type = MIME_TYPES.get(requested_path.suffix, 'application/octet-stream')
-                        response += (f'Content-Length: {len(content)}\r\nContent-Type: {content_type}\r\n')
-                        
-                        last_modified_timestamp = os.stat(requested_path).st_mtime
-                        modified_utc = datetime.fromtimestamp(last_modified_timestamp, tz=timezone.utc)
-                        last_modified = modified_utc.strftime("%A, %d %B %Y %H:%M:%S GMT")
-
-                        response += (f'Last-Modified: {last_modified}\r\n')
-                        
-                        response += (f'ETag: "{current_etag}"\r\n')
-
-                        response += (f'\r\n')
-                        response = response.encode('utf-8')
-                        
-                        return response
-                    except:
-                        return responses.internal_server_error()
-            except FileNotFoundError:
-                return responses.not_found()
-        else:
-            return responses.not_found()
-    else:
+    if not (BASE_DIR in requested_path.parents or BASE_DIR == requested_path):
         return responses.forbidden()
+    if not (requested_path.exists() and requested_path.is_file()):
+        return responses.not_found()
+
+    try:
+        with open(requested_path, 'rb') as f:
+            try:
+                content = f.read()
+                
+                now = datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
+
+                current_etag = get_etag(str(requested_path))
+                user_etag = headers.get('if-none-match')
+                if user_etag and user_etag[1:-1] == current_etag:
+                    return (f"HTTP/1.1 304 Not Modified\r\nDate: {now}\r\nServer: David's server\r\n\r\n").encode('utf-8')
+
+                response = (f"HTTP/1.1 200 OK\r\nDate: {now}\r\nDavid's server\r\n")
+                
+                if headers.get('connection', '') == 'close':
+                    response += (f'Connection: Close\r\n')
+                else:
+                    response += (f'Connection: Keep-Alive\r\n')
+                
+                content_type = MIME_TYPES.get(requested_path.suffix, 'application/octet-stream')
+                response += (f'Content-Length: {len(content)}\r\nContent-Type: {content_type}\r\n')
+                
+                last_modified_timestamp = os.stat(requested_path).st_mtime
+                modified_utc = datetime.fromtimestamp(last_modified_timestamp, tz=timezone.utc)
+                last_modified = modified_utc.strftime("%A, %d %B %Y %H:%M:%S GMT")
+
+                response += (f'Last-Modified: {last_modified}\r\n')
+                
+                response += (f'ETag: "{current_etag}"\r\n')
+
+                response += (f'\r\n')
+                response = response.encode('utf-8')
+                
+                return response
+            except:
+                return responses.internal_server_error()
+    except FileNotFoundError:
+        return responses.not_found()
--- handlers/post.py
+++ handlers/post.py
@@ -14,47 +14,47 @@ def post(path: str, headers: dict[str, str], body: bytes = b'') -> bytes:
 
     requested_path = (BASE_DIR / path).resolve()
 
-    if BASE_DIR in requested_path.parents or BASE_DIR == requested_path:
-        if API_DIR in requested_path.parents or API_DIR == requested_path:
-            if requested_path.exists() and requested_path.is_file():
-                try:
-                    with open(requested_path, 'ab') as f:
-                        try:
-                            f.write(body)
-                            f.write(('\r\n').encode('utf-8'))
-
-                            f.flush()
-
-                            with open(requested_path, 'rb') as f:
-                                content = f.read()
-                            etag = get_etag(str(requested_path))
-
-                            now = datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
-
-                            response = (
-                                f'HTTP/1.1 201 Created\r\n'
-                                f"Server: David's server\r\n"
-                                f'Date: {now}\r\n'
-                                f'Location: {path}\r\n'
-                                f'Content-Type: {MIME_TYPES.get(requested_path.suffix, 'application/octet-stream')}\r\n'
-                                f'ETag: {etag}\r\n'
-                                )
-                            
-                            if headers.get('connection', '') == 'close':
-                                response += (f'Connection: Close\r\n')
-                            else:
-                                response += (f'Connection: Keep-Alive\r\n')
-
-                            response += ('\r\n')
-                            
-                            return response.encode('utf-8')
-                        except:
-                            return responses.internal_server_error()
-                except FileNotFoundError:
-                    return responses.not_found()
-            else:
-                return responses.not_found()
-        else:
-            return responses.method_not_allowed()
-    else:
+    if not (BASE_DIR in requested_path.parents or BASE_DIR == requested_path):
         return responses.forbidden()
+    
+    if not (API_DIR in requested_path.parents or API_DIR == requested_path):
+        return responses.method_not_allowed()
+    
+    if not (requested_path.exists() and requested_path.is_file()):
+        return responses.not_found()
+
+    try:
+        with open(requested_path, 'ab') as f:
+            try:
+                f.write(body)
+                f.write(('\r\n').encode('utf-8'))
+
+                f.flush()
+
+                with open(requested_path, 'rb') as f:
+                    content = f.read()
+                etag = get_etag(str(requested_path))
+
+                now = datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
+
+                response = (
+                    f'HTTP/1.1 201 Created\r\n'
+                    f"Server: David's server\r\n"
+                    f'Date: {now}\r\n'
+                    f'Location: {path}\r\n'
+                    f'Content-Type: {MIME_TYPES.get(requested_path.suffix, 'application/octet-stream')}\r\n'
+                    f'ETag: {etag}\r\n'
+                    )
+                
+                if headers.get('connection', '') == 'close':
+                    response += (f'Connection: Close\r\n')
+                else:
+                    response += (f'Connection: Keep-Alive\r\n')
+
+                response += ('\r\n')
+                
+                return response.encode('utf-8')
+            except:
+                return responses.internal_server_error()
+    except FileNotFoundError:
+        return responses.not_found()
