460fc8a8e919ff3f41ac68c7beda12ab0e6ef0fc
David Meng <davidmenggx@gmail.com>
2026-01-30 16:20:46 -0800

    Update test for consistency

--- tests/test_get_request_line.py
+++ tests/test_get_request_line.py
@@ -2,13 +2,11 @@ import pytest
 
 from utilities import get_request_line
 
-VALID_REQUEST = "POST /api/public_file.txt HTTP/1.1\r\nHost: api.example.com\r\nContent-Type: application/json\r\nContent-Length: 73"
-
 def test_valid_request():
-    assert get_request_line(VALID_REQUEST) == (('POST', '/api/public_file.txt', 'HTTP/1.1'), ['Host: api.example.com', 'Content-Type: application/json', 'Content-Length: 73'])
-
-INVALID_REQUEST = "POST /api/public_file.txt\r\nHost: api.example.com\r\nContent-Type: application/json\r\nContent-Length: 73"
+    valid_request = "POST /api/public_file.txt HTTP/1.1\r\nHost: api.example.com\r\nContent-Type: application/json\r\nContent-Length: 73"
+    assert get_request_line(valid_request) == (('POST', '/api/public_file.txt', 'HTTP/1.1'), ['Host: api.example.com', 'Content-Type: application/json', 'Content-Length: 73'])
 
 def test_invalid_request_line():
+    invalid_request = "POST /api/public_file.txt\r\nHost: api.example.com\r\nContent-Type: application/json\r\nContent-Length: 73"
     with pytest.raises(ValueError):
-        get_request_line(INVALID_REQUEST)
+        get_request_line(invalid_request)
